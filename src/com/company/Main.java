d5aa2a16097ad13627c83c5ca89108b3ba71d41c
DeoAlbert <88679442+DeoAlbert@users.noreply.github.com>
2022-04-27 02:53:31 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,11 @@
-package com.company;
-
-public class Main {
-
+public class position {
     public static void main(String[] args) {
-        double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
-        System.out.println("The object's position after " + fallingTime +
-                " seconds is " + finalPosition + " m.");
+        double g = -9.81;              // Earth's gravity in m/s^2
+        double v = 0.0;               //initial speed
+        double t = 10.0;            //time of flight
+        double x = 0.0;             //initial position
+        x = 0.5 * g * t * t + v * t + x;      //Final position
+        System.out.println("The object's position after " + t +
+                " seconds is " + x + " m.");
     }
 }
